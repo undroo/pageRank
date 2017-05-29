864e5984fac4aef2febf94ac6a4d9e68682ae0d7
michseeto <mich05@live.com>
2017-05-30 01:26:42 +1000

    Create adjlist.h

---
+++ adjlist.h
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <assert.h>
+#include <string.h>
+#include "set.h"
+
+typedef struct AdjListRep *AdjList;
+
+AdjList newAdjList();
+void disposeAdjList(AdjList);
+void insertAdjListNode(AdjList,char *);
+void insertAdjListURL(AdjList,char *,char *);
+int  inAdjList(AdjList,char *);
+int  nAdjList(AdjList);
+void showAdjList(AdjList);
