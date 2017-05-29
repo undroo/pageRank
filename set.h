247eb500b0401908b6693ea0af9b936d1375920f
michseeto <mich05@live.com>
2017-05-29 23:24:52 +1000

    Update set.h

--- set.h
+++ set.h
@@ -1,16 +1,18 @@
-//set.h
-//Based off code written by John Shepherd, September 2015
+// set.h ... interface to simple Set of Strings
+// Based off code written by John Shepherd, September 2015
 
-
+#ifndef SET_H
+#define SET_H
 
 typedef struct SetRep *Set;
 
+// Function signatures
+
 Set newSet();
-void freeSet(Set);
+void disposeSet(Set);
 void insertInto(Set,char *);
-//void dropFrom(Set,char *);
-//int  isElem(Set,char *);
-//int  nElems(Set);
-//void showSet(Set);
+int  isElem(Set,char *);
+int  nElems(Set);
+void showSet(Set);
 
-
+#endif
