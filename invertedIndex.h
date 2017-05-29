64a1b5311b783956fb03903ebc3c788b4f330933
michseeto <mich05@live.com>
2017-05-30 03:40:55 +1000

    Update invertedIndex.h

--- invertedIndex.h
+++ invertedIndex.h
@@ -1,11 +1,7 @@
-// invertedIndex.h
-// Written by Michelle Seeto (5061204)
-// Assignment 2 COMP1927 S1,17
-
 #ifndef INVERTEDINDEX_H
 #define INVERTEDINDEX_H
 
-typedef struct AdjListRep *AList;
-typedef struct AdjListNode *ALNode;
+AdjList GetInvertedList(Set);
+char *normalise(char *);
 
 #endif
