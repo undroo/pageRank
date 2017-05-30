2b16c33e58adb5af8aed09a25219d438fd2e0d7f
michseeto <mich05@live.com>
2017-05-30 20:14:50 +1000

    Update searchPagerank.c

--- searchPagerank.c
+++ searchPagerank.c
@@ -14,6 +14,7 @@ int main(int argc, char **argv)
     FILE *fp1, *fp2;
     char url[BUFSIZE];
     Set inputs = newSet();
+    Set results = newSet();
     
     int j = 1;
     for (j = 1; j < argc; j++)
@@ -25,40 +26,46 @@ int main(int argc, char **argv)
     fp1 = fopen("pagerankList.txt", "r");
     assert(fp1 != NULL);
     
-    int i = 0, n = 1;
+    int i = 0;
     while ((fscanf(fp1, "%s", url) != EOF) && i < 10)
     {
         if (strstr(normalise(url),"url"))
         {
-            fp2 = fopen("invertedIndex.txt", "r");
-            assert(fp2 != NULL);
-
-            for (j = 0; j < nElems(inputs) && n == 1; j++)
-                n = searchLine(fp2,getValue(inputs,j),url);
-            
-            if (n == 1)
+            for (j = 0; j < nElems(inputs); j++)
             {
-                puts( url );
-                i++;
+                fp2 = fopen("invertedIndex.txt", "r");
+                assert(fp2 != NULL);
+                if (!searchFile(fp2,getValue(inputs,j),url))
+                    break;
+                if (j == nElems(inputs) - 1)
+                {
+                    insertInto(results,url);
+                    i++;
+                }
+                fclose(fp2);
             }
-            n = 1;
-            fclose(fp2);
         }
     }
     fclose(fp1);
+    
+    for (j = 0; j < nElems(results); j++)
+        puts(getValue(results,j));
+    
 }
 
 // searchLine(FILE *,char *,char *)
 // - search file for word and url
 // - return 1 if word and url are on same line
 // - else return 0
-int searchLine(FILE *fp, char * word, char * url)
+int searchFile(FILE *fp, char * word, char * url)
 {
     char line[BUFSIZE];
     while (fgets(line,BUFSIZE,fp) != NULL)
     {
         if (strstr(line,word) != NULL && strstr(line,url) != NULL) // word & url are both in line
             return 1;
+        else if (strstr(line,word) != NULL && strstr(line,url) == NULL)
+            return 0;
     }
     return 0;
 }
