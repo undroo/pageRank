355de61d70beba7e9c3663e5155e68aacd496f5b
michseeto <mich05@live.com>
2017-05-31 01:55:12 +1000

    Update set.c

--- set.c
+++ set.c
@@ -24,12 +24,14 @@ typedef struct SetRep {
 Set newSet();
 void disposeSet(Set);
 void insertInto(Set,char *);
+void insertIntoAlpha(Set,char *);
 int  isElem(Set,char *);
 int  nElems(Set);
 char *getValue(Set,int);
 
 static Link newNode(char *);
 static void disposeNode(Link);
+static int wordcmp(char *,char *);
 
 // newSet()
 // - create an initially empty Set
@@ -78,6 +80,44 @@ void insertInto(Set s, char *str)
 	        curr = curr->next;
 	    }
 	    prev->next = new;
+	}
+}
+
+// insertInto(Set,Str)
+// - insert Str into set S in alphabetical order
+void insertIntoAlpha(Set s, char *str)
+{
+	assert(s != NULL);
+	   
+	if (isElem(s,str)) return; 
+	
+	Link new = newNode(str);
+    s->nelems++;
+
+	if (s->elems == NULL) // list is empty
+	    s->elems = new;
+	
+	else // list has items
+	{
+	    	Link curr = s->elems, prev = s->elems;
+	    while (curr != NULL) // iterate through list until end of list is reached
+	    {
+	        if (wordcmp(curr->val,str)) // Str goes before current node
+	        {
+	            if (!strcmp(curr->val,s->elems->val)) // insert new node at start of list
+	            {
+	                new->next = curr;
+	                s->elems = new;
+	                return;
+	            }
+	            
+	            break;
+	        }
+	        prev = curr;
+	        curr = curr->next;
+	    }
+	    prev->next = new;
+	    new->next = curr;
 	}
 }
 
@@ -148,4 +188,30 @@ static void disposeNode(Link curr)
 	assert(curr != NULL);
 	free(curr->val);
 	free(curr);
+}
+
+// wordcmp(char *,char *)
+// - compare words P and Q for alphabetical order
+// - if P comes before Q, return 0
+// - if Q comes before P, return 1
+// - if words are the same, return 0
+static int wordcmp(char *p, char *q)
+{   
+    if (!strcmp(p,q))
+        return 0;
+    
+    while (*p != '\0' && *q != '\0')
+    {
+        if ((int)(*p) > (int)(*q))
+            return 1;
+        else if ((int)(*p) < (int)(*q))
+            return 0;
+        p++;
+        q++;
+    }
+    
+    if (*p == '\0')
+        return 0;
+      
+    return 1;
 }
