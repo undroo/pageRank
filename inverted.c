ffc4c513668329b6fcc428af89e1d843c8be9766
michseeto <mich05@live.com>
2017-06-03 01:20:28 +1000

    Update inverted.c

--- inverted.c
+++ inverted.c
@@ -14,16 +14,15 @@
 
 #define BUFSIZE 1024
 
-Set GetCollection();
 AdjList GetInvertedList(Set);
 char *normalise(char *);
 
 int main()
 {
-    Set s = GetCollection();
-    AdjList al = GetInvertedList(s);
+    Set s = GetCollection(); // get urls from collection.txt
+    AdjList al = GetInvertedList(s); // make an inverted list from the given urls
 
-    FILE * ptr = fopen("invertedIndex.txt", "w");
+    FILE * ptr = fopen("invertedIndex.txt", "w"); // print the adjacency list to a text file
     assert( ptr != NULL );
     showAdjList(ptr,al);
     
@@ -42,22 +41,22 @@ AdjList GetInvertedList(Set URLlist) {
     char line[BUFSIZE];
     FILE *ptr;
     
-    AdjList invertedIndex = newAdjList();
+    AdjList invertedIndex = newAdjList(); // create empty adjacency list
     
-    for (int n = 0; n < nElems(URLlist); n++)
+    for (int n = 0; n < nElems(URLlist); n++) // iterate through url list
     {
-        strcpy(url,getValue(URLlist,n));
-        strcpy(fullurl,url);
-        ptr = fopen(strcat(fullurl,".txt"), "r");
+        strcpy(url,getValue(URLlist,n)); // copy url from list to "url" string
+        strcpy(fullurl,url); // copy from "url" string to "fullurl" string
+        ptr = fopen(strcat(fullurl,".txt"), "r"); // add ".txt" to the end of fullurl, then open given text file
         assert( ptr != NULL );
 
-        while (strstr(fgets(line, BUFSIZE, ptr), "Section-2") == NULL) { }
+        while (strstr(fgets(line, BUFSIZE, ptr), "Section-2") == NULL) { } // scan line by line until section 2
         
-        while (fscanf(ptr, "%s", word) != EOF)
+        while (fscanf(ptr, "%s", word) != EOF) // scan word by word until end of file
         {
-            if (strstr(word, "#end") != NULL ) break;
-            insertAdjListNodeAlpha(invertedIndex,normalise(word));
-            insertAdjListURL(invertedIndex,normalise(word),url);
+            if (strstr(word, "#end") != NULL ) break; // if end of section 2 is reached, break out of fscanf
+            insertAdjListNodeAlpha(invertedIndex,normalise(word)); // create a node for each (new) word in text file
+            insertAdjListURL(invertedIndex,normalise(word),url); // add url to the set of given word
         }
         
         fclose(ptr);
@@ -66,6 +65,8 @@ AdjList GetInvertedList(Set URLlist) {
     return invertedIndex;
 }
 
+// normalise(char *)
+// - normalise words by making everything lowercase and omitting punctuation (except for hyphens)
 char *normalise(char *word)
 {
     char *url;
