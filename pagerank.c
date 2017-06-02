6357586f1a7f2059e830690273364830f35d0ee6
michseeto <mich05@live.com>
2017-06-03 01:02:21 +1000

    Update pagerank.c

--- pagerank.c
+++ pagerank.c
@@ -9,45 +9,33 @@
 #include "set.h"
 #include "adjlist.h"
 #include "readData.h"
+#include "setInt.h"
 
 
 #define BUFSIZE 1024
-
-float **pageRankArray(int, int, int, Set, AdjList);
-float pageRank(int, int, int, char *, Set, AdjList);
-float calcDiffPR(char *url, AdjList M);
-
 
 int main(int argc, char **argv){
 
 	if (argc != 4) return EXIT_FAILURE;
 	
-	
-	double d = atof(argv[1]);
+	double d = atof(argv[1]); // collect input numbers
 	double diffPR = atof(argv[2]);
 	int maxIter = atoi(argv[3]);
 	
-	Set urlSet;
-	urlSet = GetCollection();
-	AdjList M;
-	M = buildList(urlSet);
+	Set urls = GetCollection(); // collect urls from collection.txt
+	AdjList M = buildM(urls); // build adjacency list to keep track of which urls point where
+	SetInt L = buildL(urls); // build list to keep track of urls' out degrees
+	SetInt rank = newSetInt(); // create set to store ranking of urls
 	
-	showSet(urlSet);
-	showAdjList(stdout, M);
-	
-	int N = nElems(urlSet);
-	
-	double PR[N][maxIter];
-	
-	int i, j;
-	
-    int iteration = 0;
-	double diff = (double)diffPR;
-    double a = (double)(1-d)/(double)N;
-    
+	int N = nElems(urls);
+	double PR[N][maxIter]; // 2D matrix to store page ranks
+	int i, j;  
     char url[BUFSIZE];
     int index;
+    int iteration = 0;
+	double diff = diffPR;
     
+    // commence PageRank algorithm
 	for (i = 0; i < N; i++)
 	    PR[i][0] = 1/(double)N;
 	    
@@ -64,23 +52,46 @@ int main(int argc, char **argv){
 	        {
 	            strcpy(url,getValue(m,j)); // get j'th url of M list
 	            index = findAdjListNode(M,url); // get index of url
-	            sum += (double)PR[index][iteration-1]/(double)nElems(getSet(M,j));
+	            sum += PR[index][iteration-1]/(double)getValueInt(L,index);
 	        }
 	        
-	        PR[i][iteration] = (double)a + (double)d*(double)sum;
-	    
-	        diff = 0;
+	        PR[i][iteration] = (1-d)/N + d*sum; // calculate PageRank and store in array
+	        
+	        diff = 0; // calculate diff
 	        for (j = 0; j < N; j++)
 	            diff += fabs(PR[i][iteration]-PR[i][iteration-1]);
 	    }
 	}
+	// end PageRank algorithm
 	
-	for (i = 0; i < N; i++)
-	    printf( "[%d]: %f\n", i, PR[i][iteration]);
-	
-	
-	disposeSet(urlSet);
+	// commence ordering urls in descending order
+	insertIntoInt(rank,0); // Rank list stores the url indices, when the urls are in descending order
+    for (i = 1; i < N; i++)
+    {
+        for (j = i-1; j >= 0; j--) // Iterate through already inserted nodes until reaches a node of greater PR value
+        {
+            index = getValueInt(rank,j);
+            double n = PR[i][iteration];
+            double m = PR[index][iteration];
+            if(n <= m)
+            {
+                insertAfterInt(rank,i,index);
+                break;
+            } 
+        }
+    }
+    // end ordering
+    
+    for (i = 0; i < N; i++)
+    {
+        index = getValueInt(rank,i);
+        printf( "%s, %d, %0.8f\n", getValue(urls,index), getValueInt(L, index), PR[index][iteration]);
+    }
+    
+	disposeSet(urls);
 	disposeAdjList(M);
+	disposeSetInt(L);
+	disposeSetInt(rank);
 	
 	return 0;
 }
