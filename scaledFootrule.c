a099f2a02b7853256e98088570a76fb569834d46
michseeto <mich05@live.com>
2017-06-02 14:13:15 +1000

    Update scaledFootrule.c

--- scaledFootrule.c
+++ scaledFootrule.c
@@ -6,13 +6,10 @@
 #include <math.h>
 #include "setInt.h"
 #include "set.h"
+#include "scaledFootrule.h"
 
 #define BUFSIZE 1024
 #define MAXURL 100
-
-int calculateP(int *,int,SetInt,SetInt);
-double calculateW(int *,SetInt,int,int);
-void printOrderedList(Set,SetInt);
 
 int main(int argc, char **argv)
 {
@@ -22,50 +19,41 @@ int main(int argc, char **argv)
     
     int i, k;
     
-    for (i = 0; i < MAXURL; i++)
+    for (i = 0; i < MAXURL; i++) // initialise the 2D position array so each term is 0
     {
         for (k = 0; k < argc-1; k++)
             position[i][k] = 0;
     }
     
-    SetInt listsize = newSetInt();
-    SetInt p = newSetInt();
-    Set unionlist = newSet();
+    // create sets to keep track of:
+    SetInt listsize = newSetInt(); // size of input lists
+    SetInt p = newSetInt(); // values of p
+    Set unionlist = newSet(); // input urls
     
-    for (i = 1; i < argc; i++) // create union of two lists and store list size and url positions
+    for (i = 1; i < argc; i++) // open each input text file
     {
         fp1 = fopen(argv[i], "r");
         assert(fp1 != NULL);
         int j = 0;
         
-        while (fscanf(fp1, "%s", url) != EOF)
+        while (fscanf(fp1, "%s", url) != EOF) // scan through text file
         {
-            j++;
-            if (!isElem(unionlist,url))
-            {
+            j++; // keep track of position in text file
+            if (!isElem(unionlist,url)) // insert url into set if not already existing
                 insertInto(unionlist,url);
-                printf( "Insert %s\n", url);
-                printf( "New node is element no. %d\n", findNode(unionlist,url));
-            }
-            k = findNode(unionlist,url);
+            k = findNode(unionlist,url); // find the index of the inserted/already existing url
             if (k != -1)
-            {
-                position[k][i-1] = j;
-                printf( "Position of %s in txt no. %d is %d\n", url, i, j);
-            }
+                position[k][i-1] = j; // update position array
         }
-        insertIntoInt(listsize,j);
-        printf( "Size of txt no. %d is %d\n", i, j);
+        insertIntoInt(listsize,j); // store final list size in set
         fclose(fp1);
     }
     
     double w = 0;
-    for (i = 0; i < nElems(unionlist); i++)
+    for (i = 0; i < nElems(unionlist); i++) // calculate w by iterating through each url in set
     {
-        printf( "Calculate P of " );
-        showNode(unionlist,i);
-        insertIntoInt(p,calculateP(position[i], nElems(unionlist), listsize,p));
-        w += calculateW(position[i], listsize, getValueInt(p,i), nElems(unionlist));
+        insertIntoInt(p,calculateP(position[i], nElems(unionlist), listsize,p)); // calculate p using algorithm below, and insert into p set
+        w += calculateW(position[i], listsize, getValueInt(p,i), nElems(unionlist)); // calculate individual term and increment w
     }
     
     printf( "%.6f\n", w);
@@ -76,6 +64,8 @@ int main(int argc, char **argv)
     disposeSetInt(p);
 }
 
+// printOrderedList(Set,SetInt)
+// - takes the set of urls and set of p values and prints the urls in order of their positions
 void printOrderedList(Set unionlist, SetInt p)
 {
     int i;
@@ -85,13 +75,12 @@ void printOrderedList(Set unionlist, SetInt p)
 }
 
 // getW(Set)
+// - calculate each individual term in w for each url in each list
 double calculateW(int * position, SetInt listsize, int p, int n)
 {
     double a, b;
     double sum = 0;
     int i;
-
-    printf( "Calculate W\n" );
 
     for (i = 0; i < nElemsInt(listsize); i++)
     {
@@ -100,14 +89,17 @@ double calculateW(int * position, SetInt listsize, int p, int n)
             a = (double)position[i]/(double)getValueInt(listsize,i);
             b = (double)p/(double)n;
             sum += fabs(a-b);
-            printf( "a = %0.3f, b = %0.3f, c = %0.3f, sum = %0.3f\n", a, b, fabs(a-b), sum);
         }
-        else
-            printf( "Did not calculate w\n" );
     }
     return sum;
 }
 
+// calculateP(int *,int,SetInt,SetInt)
+// This algorithm aims to minimise the value of w
+// It finds the integer value of p that will make some terms in w become zero
+// It calculates multiple values of p based on the url's position in the given lists
+// It eliminates the non-integer solutions
+// If no integer solutions are found, a value of p will be allocated that has not already been used
 int calculateP(int * position, int n, SetInt listsize, SetInt plist)
 {   
     double x[MAXURL];
@@ -118,29 +110,21 @@ int calculateP(int * position, int n, SetInt listsize, SetInt plist)
     {
         if (position[i] != 0)
         {
-            x[i] = (double)(position[i]*n)/(double)getValueInt(listsize,i);
-            printf( "x[%d] = %0.3f\n", i, x[i]);
-            printf( "compare: %0.3f and %0.3f\n", fabs(round(x[i])-x[i]), min);
-            if (!isElemInt(plist,round(x[i])))
+            x[i] = (double)(position[i]*n)/(double)getValueInt(listsize,i); // calculate the value of p for each list the url is in
+            if (!isElemInt(plist,round(x[i]))) // check if the calculated value has already been allocated to another url
             {
-                if (fabs(round(x[i])-x[i]) < min)
+                if (fabs(round(x[i])-x[i]) < min) // check if the calculate value is the closest so far to an integer
                 {
-                    printf( "%0.3f better\n", x[i]);
-                    min = fabs(round(x[i])-x[i]);
-                    p = round(x[i]);
+                    min = fabs(round(x[i])-x[i]); // if so, update min for subsequent comparisons
+                    p = round(x[i]); // allocated rounded value to p
                 }
-                else
-                    printf( "%0.3f not better\n", x[i]);
+
             }
-            else
-                printf( "%0.3f already in list\n", round(x[i]));
         }
-        else
-            printf( "Did not calculate p\n");
     }
     
-    if (p = -1)
+    if (p == -1) // check if p has been allocated
+        p = getInt(plist); // if not, generate int value that has not already been allocated
     
-    printf( "p = %d\n", p);
     return p;
 }
