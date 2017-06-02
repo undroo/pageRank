8e79877492d58a8c49ac54a58a0793952175a492
michseeto <mich05@live.com>
2017-06-02 14:13:35 +1000

    Add files via upload

---
+++ scaledFootrule.h
@@ -0,0 +1,8 @@
+#ifndef SCALEDFOOTRULE_H
+#define SCALEDFOOTRULE_H
+
+int calculateP(int *,int,SetInt,SetInt);
+double calculateW(int *,SetInt,int,int);
+void printOrderedList(Set,SetInt);
+
+#endif
